622c9c94e3fc456ebc3081e25ee7b9256775b38b
kewldan <40865857+kewldan@users.noreply.github.com>
2024-10-03 00:27:45 +0300

    fixes

--- src/aiosmsactivate/__init__.py
+++ src/aiosmsactivate/__init__.py
@@ -1,4 +1,4 @@
-from .api import get_balance, get_available_countries, get_activation_status, set_activation_status, purchase
+from .client import SmsActivate
 from .exceptions import SmsActivateException
 from .responses import PurchaseResponse, SetActivationStatusResponse
 from .types import ActivationStatus, SetActivationStatus
